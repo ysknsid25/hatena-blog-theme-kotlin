021f8fd870ecd48e51fb680b19a51003b077f315
ysknsid25 <kengo071225@gmail.com>
2026-05-06 15:49:37 +0900

    Kotlin theme
    Signed-off-by: ysknsid25 <kengo071225@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -2,20 +2,20 @@ import { defineConfig } from "vite";
 import autoprefixer from "autoprefixer";
 
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: ["scss/boilerplate.scss", "scss/boilerplate-dark.scss"],
-      output: {
-        assetFileNames: ({ name }) => name,
-      },
+    build: {
+        rollupOptions: {
+            input: ["scss/boilerplate.scss"],
+            output: {
+                assetFileNames: ({ name }) => name,
+            },
+        },
+        outDir: "build",
+        cssMinify: false,
     },
-    outDir: "build",
-    cssMinify: false,
-  },
-  css: {
-    devSourcemap: true,
-    postcss: {
-      plugins: [autoprefixer()],
+    css: {
+        devSourcemap: true,
+        postcss: {
+            plugins: [autoprefixer()],
+        },
     },
-  },
 });
